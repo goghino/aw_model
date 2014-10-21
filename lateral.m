ef2990f1031197680fb9598d4c7e6770ce9fd2fd
Juraj Kardos <xkardo00@stud.fit.vutbr.cz>
2014-10-21 10:54:44 +0200

    adding plots for lateral coeff. vs. velocity, restructuring scirpt - moving frictions coeffs. computation into lateral function

--- lateral.m
+++ lateral.m
@@ -1,5 +1,5 @@
 %Boeing 747-200 parameters
-v = 10/1.94384;     %m/s [10 knots] 
+v = 10;             %m/s [10 knots] 
 Kb = 1;             %braking ratio
 alpha = 0.01;       %[rad]
 Fz = 140000;        %[N]
@@ -24,7 +24,10 @@ Wmu_PSImax = 0.64 * Wmu_Bmax + 0.15*(Wmu_Bmax)^2;
 Wmu_PSIlim = sqrt(1-(Wmu_Beff/Wmu_Bmax*Kb)^2) * Wmu_Bmax;
 WFxb = Kb * Wmu_Beff * Fz;
 
-%DRY coefs based on speed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%DRY COEFS plot
 bmaxi = zeros(161,1);
 beffi = zeros(161,1);
 pslimi = zeros(161,1);
@@ -54,19 +57,20 @@ title('Friction coefficients vs. speed');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%DRY LATERAL FORCE
+%DRY LATERAL FORCE plot vs. yaw angle
 Fy = zeros(85,1);
 Fy1 = zeros(85,1);
 Fy2 = zeros(85,1);
 Fy3 = zeros(85,1);
 Fy4 = zeros(85,1);
 alpha = -0.08:0.005:0.34;
+v = 10;
 for i = 1:85
-    Fy(i) = lateral_friction(alpha(i),Fz,p,pr,diam,width,mu_PSImax);
-    Fy1(i) = lateral_friction(alpha(i),0.8*Fz,p,pr,diam,width,mu_PSImax);
-    Fy2(i) = lateral_friction(alpha(i),1.2*Fz,p,pr,diam,width,mu_PSImax);
-    Fy3(i) = lateral_friction(alpha(i),Fz,0.8*p,pr,diam,width,mu_PSImax);
-    Fy4(i) = lateral_friction(alpha(i),Fz,1.2*p,pr,diam,width,mu_PSImax);
+    Fy(i) = lateral_friction(0,alpha(i),Fz,p,pr,diam,width,v);
+    Fy1(i) = lateral_friction(0,alpha(i),0.8*Fz,p,pr,diam,width,v);
+    Fy2(i) = lateral_friction(0,alpha(i),1.2*Fz,p,pr,diam,width,v);
+    Fy3(i) = lateral_friction(0,alpha(i),Fz,0.8*p,pr,diam,width,v);
+    Fy4(i) = lateral_friction(0,alpha(i),Fz,1.2*p,pr,diam,width,v);
 
 end
 
@@ -74,31 +78,80 @@ alpha = alpha.*57.2957795;
 figure();
 plot(alpha,Fy,alpha,Fy1,alpha,Fy2,alpha,Fy3,alpha,Fy4);
 legend('nominal','0.8Fz','1.2Fz','0.8p','1.2p');
-xlabel('Tire angle [rad]') % x-axis label
-ylabel('friction coeffs.') % y-axis label
-title('Lateral friction coefficients vs. tire yaw angle');
+xlabel('Tire angle [deg]') % x-axis label
+ylabel('friction coeff.') % y-axis label
+title('Lateral friction coefficient vs. tire yaw angle, v=10m/s');
 
 
-%WET LATERAL FORCE
+%DRY/WET LATERAL FORCE plot vs. yaw angle
 wFy = zeros(85,1);
 wFy1 = zeros(85,1);
 wFy2 = zeros(85,1);
 wFy3 = zeros(85,1);
 wFy4 = zeros(85,1);
 walpha = -0.08:0.005:0.34;
+v = 10;
 for i = 1:85
-    wFy(i) = lateral_friction(walpha(i),Fz,p,pr,diam,width,Wmu_PSImax);
-    wFy1(i) = lateral_friction(walpha(i),0.8*Fz,p,pr,diam,width,Wmu_PSImax);
-    wFy2(i) = lateral_friction(walpha(i),1.2*Fz,p,pr,diam,width,Wmu_PSImax);
-    wFy3(i) = lateral_friction(walpha(i),Fz,0.8*p,pr,diam,width,Wmu_PSImax);
-    wFy4(i) = lateral_friction(walpha(i),Fz,1.2*p,pr,diam,width,Wmu_PSImax);
-
+    wFy(i) = lateral_friction(0,walpha(i),Fz,p,pr,diam,width,v);
+    wFy1(i) = lateral_friction(1,walpha(i),Fz,p,pr,diam,width,v);
+    wFy2(i) = lateral_friction(0,walpha(i),1.2*Fz,p,pr,diam,width,v);
+    wFy3(i) = lateral_friction(1,walpha(i),1.2*Fz,p,pr,diam,width,v);
 end
 
 walpha = walpha.*57.2957795;
 figure();
-plot(walpha,Fy,'-',walpha,wFy,'o',walpha,Fy2,'-',walpha,wFy2,'go');
+plot(walpha,wFy,'-',walpha,wFy1,'o',walpha,wFy2,'-',walpha,wFy3,'go');
 legend('nominal dry','nominal wet','1.2Fz dry','1.2Fz wet');
-xlabel('Tire angle [rad]') % x-axis label
-ylabel('friction coeffs.') % y-axis label
-title('Lateral friction coefficients in different environ. conditions');
+xlabel('Tire angle [deg]') % x-axis label
+ylabel('friction coeff.') % y-axis label
+title('Lateral friction coefficient in different environ. conditions, v=10m/s');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%DRY LATERAL FORCE plot vs. speed
+vFy = zeros(101,1);
+vFy1 = zeros(101,1);
+vFy2 = zeros(101,1);
+vFy3 = zeros(101,1);
+vFy4 = zeros(101,1);
+alpha = 5*0.01745;
+velo = 0:1:100;
+for i = 1:101
+    vFy(i) = lateral_friction(0,alpha,Fz,p,pr,diam,width,velo(i));
+    vFy1(i) = lateral_friction(0,alpha,0.8*Fz,p,pr,diam,width,velo(i));
+    vFy2(i) = lateral_friction(0,alpha,1.2*Fz,p,pr,diam,width,velo(i));
+    vFy3(i) = lateral_friction(0,alpha,Fz,0.8*p,pr,diam,width,velo(i));
+    vFy4(i) = lateral_friction(0,alpha,Fz,1.2*p,pr,diam,width,velo(i));
+
+end
+
+figure();
+plot(velo,vFy,velo,vFy1,velo,vFy2,velo,vFy3,velo,vFy4);
+legend('nominal','0.8Fz','1.2Fz','0.8p','1.2p');
+xlabel('Speed [m/s]') % x-axis label
+ylabel('friction coeff.') % y-axis label
+title('Lateral friction coefficient vs. velocity, PSI = 5deg');
+
+
+%DRY/WET LATERAL FORCE plot vs. speed
+wFy = zeros(101,1);
+wFy1 = zeros(101,1);
+wFy2 = zeros(101,1);
+wFy3 = zeros(101,1);
+wFy4 = zeros(101,1);
+walpha = 5*0.01745;
+v = 0:1:100;
+for i = 1:101
+    wFy(i) = lateral_friction(0,walpha,Fz,p,pr,diam,width,v(i));
+    wFy1(i) = lateral_friction(1,walpha,Fz,p,pr,diam,width,v(i));
+    wFy2(i) = lateral_friction(0,walpha,1.2*Fz,p,pr,diam,width,v(i));
+    wFy3(i) = lateral_friction(1,walpha,1.2*Fz,p,pr,diam,width,v(i));
+end
+
+figure();
+plot(v,wFy,'-',v,wFy1,'o',v,wFy2,'-',v,wFy3,'go');
+legend('nominal dry','nominal wet','1.2Fz dry','1.2Fz wet');
+xlabel('Speed [m/s]') % x-axis label
+ylabel('friction coeff.') % y-axis label
+title('Lateral friction coefficient in different environ. conditions, PSI=5deg');
--- lateral_friction.m
+++ lateral_friction.m
@@ -1,12 +1,32 @@
-function res = lateral_friction(yaw_alpha,Fz,p,pr,diam,width,mu_PSImax)
-%DRY RUNWAY lateral force   
-    x = Fz / (p * diam*39.3701 * sqrt(width*39.3701*diam*39.3701));
-    N = 31.3 * (width*39.3701)^2 * (p + 0.44*pr)*(1-3.17*x)*x;
-    phi = (N * yaw_alpha) / (mu_PSImax * Fz);
-    if(abs(phi)<1.5)
-        res = abs(phi - (4/27)*phi^3) * mu_PSImax;
-    else
-        res = mu_PSImax;
-    end
+%Lateral force 
+function res = lateral_friction(ENVIRON,yaw_alpha,Fz,p,pr,diam,width,v)
+Kb = 1; %full braking
+res = -10;
+
+if ENVIRON == 0
+    %DRY RUNWAY friction for longitudal braking force
+    mu_Bmax = (1 - 0.0011*p)*0.912 + v*1.94384*(-0.00079);
+    mu_Beff = mu_Bmax*0.94 - 0.03; 
+    mu_PSImax = mu_Bmax;
+    mu_PSIlim = sqrt(1-(mu_Beff/mu_Bmax*Kb)^2) * mu_Bmax;
+    Fxb = Kb * mu_Beff * Fz;
+else
+    %WET RUNWAY friction for longitudal braking force 
+    mu_Bmax = (0.91 - 0.001*p)*(1 + v*1.94384*(-0.0052));
+    mu_Beff = mu_Bmax*0.94 - 0.03; 
+    mu_PSImax = 0.64 * mu_Bmax + 0.15*(mu_Bmax)^2;
+    mu_PSIlim = sqrt(1-(mu_Beff/mu_Bmax*Kb)^2) * mu_Bmax;
+    Fxb = Kb * mu_Beff * Fz;
+end
+
+x = Fz / (p * diam*39.3701 * sqrt(width*39.3701*diam*39.3701));
+N = 31.3 * (width*39.3701)^2 * (p + 0.44*pr)*(1-3.17*x)*x;
+phi = (N * yaw_alpha) / (mu_PSImax * Fz);
+if(abs(phi)<1.5)
+    res = abs(phi - (4/27)*phi^3) * mu_PSImax;
+else
+    res = mu_PSImax;
+end
+
 end
 
