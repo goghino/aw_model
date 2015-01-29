eb35d1bbedd3ad1e90459342931f05509633ce4a
Juraj Kardos <juraj2202@gmail.com>
2015-01-29 14:29:32 +0100

    Support for real-time rendering

--- render.m
+++ render.m
@@ -2,7 +2,7 @@ scale = 0.05;
 step = 1;
 
 %Resampling time data to bigger time-step
-time = [0:0.333:100];
+time = [0:0.333:100]; %speed 10x, resaple with step 0.033 to get realtime
 x = resample(X,time);
 y = resample(Y,time);
 len = size(x.Data);
